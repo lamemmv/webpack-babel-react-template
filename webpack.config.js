f5917222f57b92fbe6f3a621b5518f6c48dc2335
lamemmv <lamemmv@gmail.com>
2017-11-26 22:31:13 +0700

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@
 // https://nodejs.org/api/path.html
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Import our plugin -> ADDED IN THIS STEP
+
 // Constant with our paths
 const paths = {
     DIST: path.resolve(__dirname, 'dist'),
+    SRC: path.resolve(__dirname, 'src'), // source folder path -> ADDED IN THIS STEP
     JS: path.resolve(__dirname, 'src/js'),
 };
 
@@ -14,5 +17,38 @@ module.exports = {
     output: {
         path: paths.DIST,
         filename: 'app.bundle.js'
+    },
+    // Tell webpack to use html plugin -> ADDED IN THIS STEP
+    // index.html is used as a template in which it'll inject bundled app.
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(paths.SRC, 'index.html'),
+        }),
+    ],
+    // Dev server configuration -> ADDED IN THIS STEP
+    // Now it uses our "src" folder as a starting point
+    // devServer: {
+    //     contentBase: paths.SRC,
+    // },
+    // Loaders configuration -> ADDED IN THIS STEP
+    // We are telling webpack to use "babel-loader" for .js and .jsx files
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: [
+                'babel-loader',
+            ],
+        }, ],
+    },
+    // Enable importing JS files without specifying their's extenstion -> ADDED IN THIS STEP
+    //
+    // So we can write:
+    // import MyComponent from './my-component';
+    //
+    // Instead of:
+    // import MyComponent from './my-component.jsx';
+    resolve: {
+        extensions: ['.js', '.jsx'],
     },
 };
