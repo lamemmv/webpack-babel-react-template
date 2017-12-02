36914143df9beefa9d5077c12543d180f780a5d0
lamemmv <lamemmv@gmail.com>
2017-12-02 22:32:23 +0700

    update routes

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 // We are using node's native package 'path'
 // https://nodejs.org/api/path.html
 const path = require('path');
-
+const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -15,7 +15,7 @@ const paths = {
 
 module.exports = {
     entry: {
-        app: path.join(paths.JS, 'app.jsx')
+        app: path.join(paths.JS, 'index.jsx')
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
@@ -25,7 +25,7 @@ module.exports = {
             title: 'Production',
             template: path.join(paths.SRC, 'index.html'),
         }),
-        new ExtractTextPlugin('style.bundle.css'),
+        new ExtractTextPlugin('style.bundle.css')
     ],
     output: {
         filename: '[name].bundle.js',
@@ -47,19 +47,100 @@ module.exports = {
             {
                 test: /\.css$/,
                 loader: ExtractTextPlugin.extract({
-                    use: ['style-loader',
-                        'css-loader'
-                    ],
+                    use: ['style-loader', 'css-loader'],
                 }),
             },
+            // Loader configurations for semantic-ui-less
+            {
+                // Load .less files from semantic-ui-less module folder
+                test: /\.less$/i,
+                include: /[/\\]node_modules[/\\]semantic-ui-less[/\\]/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        // Set importLoaders to 2, because there are two more loaders in the chain (postcss-loader
+                        // and semantic-ui-less-module-loader), which shall be used when loading @import resources
+                        // in CSS files:
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 2,
+                                sourceMap: true,
+                                minimize: true
+                            }
+                        },
+                        { loader: 'postcss-loader', options: { sourceMap: true } },
+                        {
+                            loader: 'semantic-ui-less-module-loader',
+                            options: {
+                                siteFolder: path.resolve(paths.SRC, 'semantic-ui-theme/site'),
+                                themeConfigPath: path.resolve(paths.SRC, 'semantic-ui-theme/theme.config'),
+                            }
+                        }
+                    ]
+                })
+            },
+            // for .less files:
+            // {
+            //     test: /\.less$/,
+            //     use: ExtractTextPlugin.extract({
+            //         fallback: 'style-loader',
+            //         use: [
+            //             { loader: 'css-loader' },
+            //             { loader: 'less-loader' }
+            //         ]
+            //     }),
+            //     exclude: [/[\/\\]node_modules[\/\\]semantic-ui-less[\/\\]/]
+            // },
+            // for semantic-ui-less files:
+            // {
+            //     test: /\.less$/,
+            //     use: ExtractTextPlugin.extract({
+            //         fallback: 'style-loader',
+            //         use: [
+            //             { loader: 'css-loader' },
+            //             {
+            //                 loader: 'semantic-ui-less-module-loader'
+            //             }
+            //         ]
+            //     }),
+            //     include: [/[\/\\]node_modules[\/\\]semantic-ui-less[\/\\]/]
+            // },
             // File loader for image assets 
             // We'll add only image extensions, but you can things like svgs, fonts and videos
             {
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jpeg|jpg|gif|ttf|eot|svg)$/,
                 use: [
-                    'file-loader',
+                    'file-loader?name=[name].[ext]?[hash]',
                 ],
             },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/fontwoff'
+            },
+
+            // loader for static assets
+            // {
+            //     test: /\.(png|jpg|jpeg|gif|svg)$/,
+            //     use: {
+            //         loader: 'url-loader',
+            //         options: {
+            //             limit: 10240,
+            //             absolute: true,
+            //             name: 'images/[path][name]-[hash:7].[ext]'
+            //         }
+            //     },
+            //     include: [path.join(__dirname, 'src'), /[\/\\]node_modules[\/\\]semantic-ui-less[\/\\]/]
+            // }, {
+            //     test: /\.(woff|woff2|ttf|svg|eot)$/,
+            //     use: {
+            //         loader: 'url-loader',
+            //         options: {
+            //             limit: 10240,
+            //             name: 'fonts/[name]-[hash:7].[ext]'
+            //         }
+            //     },
+            //     include: [path.join(__dirname, 'src'), /[\/\\]node_modules[\/\\]semantic-ui-less[\/\\]/]
+            // }
         ],
     },
     // Enable importing JS files without specifying their's extenstion 
@@ -71,5 +152,9 @@ module.exports = {
     // import MyComponent from './my-component.jsx';
     resolve: {
         extensions: ['.js', '.jsx'],
+
+        // alias: {
+        //     '../../theme.config$': path.join(__dirname, 'semantic-theme/theme.config')
+        // }
     },
 };
